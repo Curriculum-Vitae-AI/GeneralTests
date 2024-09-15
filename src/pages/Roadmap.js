fa757ae352394ec3d0649ad14ccc138329747d49
erik-akio <erikmatsumoto18@gmail.com>
2024-09-15 17:28:12 -0300

    feat: adicionado Roadmap

--- src/pages/Roadmap.js
+++ src/pages/Roadmap.js
@@ -2,7 +2,16 @@ import "../styles/pages/Roadmap.css";
 
 function Roadmap(){
     return(
-        <div></div>
+        <div className="Roadmap">
+            <div>
+                <h1>Vaga</h1>
+                <textarea placeholder="Qual vaga quer se candidatar?"></textarea>
+            </div>
+            <div className="botoes">
+                <button className="btnVoltar">VOLTAR</button>
+                <button className="btnDownload">DOWNLOAD</button>
+            </div>
+        </div>
     );
 }
 
