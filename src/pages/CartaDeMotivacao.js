d65c7cef2c95520aafaedc4628de618c3e5c107f
erik-akio <erikmatsumoto18@gmail.com>
2024-09-15 17:37:24 -0300

    feat: adicionado Carta de motivação e Vagas

--- src/pages/CartaDeMotivacao.js
+++ src/pages/CartaDeMotivacao.js
@@ -2,7 +2,27 @@ import "../styles/pages/CartaDeMotivacao.css";
 
 function CartaDeMotivacao(){
     return(
-        <div></div>
+        <div className="Roadmap">
+            <div>
+                <div>
+                    <h1>Nome da empresa</h1>
+                    <input placeholder="Qual é o nome da empresa?"/>
+                </div>
+                <div>
+                    <h1>Cargo desejado</h1>
+                    <input placeholder="Qual é a vaga?"/>
+                </div>
+                <div>
+                    <h1>Motivo</h1>
+                    <textarea placeholder="Escreva o motivo de querer esta vaga"></textarea>
+                </div>
+                
+            </div>
+            <div className="botoes">
+                <button className="btnVoltar">VOLTAR</button>
+                <button className="btnDownload">DOWNLOAD</button>
+            </div>
+        </div>
     );
 }
 
