2810a4e34e0d3876cc6a225ed0425df2a5107ac5
erik-akio <erikmatsumoto18@gmail.com>
2024-09-15 15:16:17 -0300

    feat: adicionado card home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,24 @@
 import "../styles/pages/Home.css";
+import imgRoadmap from "../assets/Home/imgRoadmap.svg"
 
 function Home(){
     return(
-        <div>
-            <p>Home</p>
+        <div className="Home">
+            <div className="card">
+                <div className="esquerda">
+                    <h1>ROADMAP</h1>
+                    <p>Com nosso gerador de roadmaps
+                       você somente precisa indicar a sua
+                       vaga desejada e ele irá montar um
+                       plano de estudos para que você
+                       possa focar somente no aprendizado.
+                    </p>
+                    <button>GERAR</button>
+                </div>
+                <div className="direita">
+                    <img src={imgRoadmap} className="imgRoadmap"/>
+                </div>
+            </div>
         </div>
     );
 }
