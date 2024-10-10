a8af23c33cbc2717ff9198b0c517530c58fae9d0
Vinicius Peralta <83187126+ViniPeralta@users.noreply.github.com>
2024-10-10 20:27:23 -0300

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,7 +3,9 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (req, res) => {
+    console.log('Oi');
     res.send('Hello World')
+    console.log('Tchau');
 });
 
 export default router;
